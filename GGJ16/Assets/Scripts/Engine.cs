03cda0bc918db415c55b7d1c94cc583a9efeb596
Pete Hufnagel <petehufnagel@hotmail.com>
2016-01-30 00:08:20 -0800

    Starting prototyping cards.

--- GGJ16/Assets/Scripts/Engine.cs
+++ GGJ16/Assets/Scripts/Engine.cs
@@ -2,20 +2,19 @@ using UnityEngine;
 
 public class Engine : MonoBehaviour
 {
-	
-
-	Vector2 acceleration = Vector2.zero;
-
-	public float inputSpeed = 1f;
+    public PhysicsObj physics = new PhysicsObj();
 	public Vector2 inputForce = Vector2.zero;
 
 	public void Tick(float deltaTime)
 	{
-
-		acceleration += inputForce.normalized*inputSpeed*deltaTime;
-		Vector3 deltaPosition = new Vector3(acceleration.x, 0, acceleration.y);
-		this.transform.position = this.transform.position + deltaPosition;
-
-		acceleration = Vector2.zero;
 	}
+
+    public void FixedUpdate()
+    {
+        this.physics.FixedUpdate(new Vector3(inputForce.normalized.x, 0, inputForce.normalized.y));
+        this.transform.position = this.physics.position;
+
+        DashCard dc = new DashCard();
+        dc.FixedUpdate(this);
+    }
 }
--- GGJ16/Assets/Scripts/Physics/PhysicsObj.cs
+++ GGJ16/Assets/Scripts/Physics/PhysicsObj.cs
@@ -1,29 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PhysicsObj : MonoBehaviour
+public class PhysicsObj
 {
     public Vector3 position = Vector3.zero;
 
     public Vector3 velocity = Vector3.zero;
-    public float drag = 1;
+    public float drag = 2.0f;
 
-    public Vector3 input = Vector3.zero;
     public float inputPower = 1;
 
-
-    // Use this for initialization
-    void Start()
-    {
-	}
-	
-	void FixedUpdate()
+    public void FixedUpdate(Vector3 input)
     {
         Vector3 moveDelta = Vector3.zero;
-        moveDelta += this.input * Time.deltaTime;
+        
+        moveDelta += input * Time.deltaTime;
+        moveDelta += this.velocity * Time.deltaTime;
 
-
+        this.velocity -= this.velocity * Time.deltaTime * drag;
 
         this.position += moveDelta;
 	}
+}
+
+public interface IActionCard
+{
+    void FixedUpdate(Engine engine);
+}
+
+public class DashCard
+{
+    public void FixedUpdate(Engine engine)
+    {
+        engine.physics.velocity += Vector3.forward;
+    }
 }
