69a6b6ef369109c8eaefb43b069d5daa1255920b
Pete Hufnagel <petehufnagel@hotmail.com>
2016-01-29 23:33:47 -0800

    Starting on physics

---
+++ GGJ16/Assets/Scripts/Physics/PhysicsObj.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhysicsObj : MonoBehaviour
+{
+    public Vector3 position = Vector3.zero;
+
+    public Vector3 velocity = Vector3.zero;
+    public float drag = 1;
+
+    public Vector3 input = Vector3.zero;
+    public float inputPower = 1;
+
+
+    // Use this for initialization
+    void Start()
+    {
+	}
+	
+	void FixedUpdate()
+    {
+        Vector3 moveDelta = Vector3.zero;
+        moveDelta += this.input * Time.deltaTime;
+
+
+
+        this.position += moveDelta;
+	}
+}
