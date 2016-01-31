c91da7f1169d82bcd89106a239a94c7e53178796
threeup <threeup@gmail.com>
2016-01-31 00:41:02 -0800

    material swaps

--- GGJ16/Assets/Scripts/Action/ActionSequencer.cs
+++ GGJ16/Assets/Scripts/Action/ActionSequencer.cs
@@ -24,7 +24,7 @@ public class ActionSequencer : MonoBehaviour
         ForwardDash forwardDash = new ForwardDash();
         forwardDash.source = gameObject;
         forwardDash.force = 3f;
-        forwardDash.duration = 0.2f;
+        forwardDash.duration = 0.4f;
         sequences[0].actions.Add(forwardDash);
 
         Grab grabAction = new Grab();
