307eceb4f09996c594e117d3f04cd5df6d97c095
Louis Carl Pepin <p.louis4@gmail.com>
2016-01-31 16:21:01 -0800

    Name the players.

--- GGJ16/Assets/Scripts/UI/SequenceHud.cs
+++ GGJ16/Assets/Scripts/UI/SequenceHud.cs
@@ -20,8 +20,6 @@ public class SequenceHud : MonoBehaviour
 		{
 			if (sequencer.CurrentAction != null)
 			{
-				GameObject.Find("Ritual_" + PlayerIndex).SetActive(true);
-
 				string textName = string.Format("Ritual_{0}/ButtonLabel_{1}", PlayerIndex, sequencer.SequenceIndex);
 				GameObject.Find(textName).GetComponent<Text>().text = sequencer.CurrentAction.name;
 				string imageName = string.Format("Ritual_{0}/Image_{1}", PlayerIndex, sequencer.SequenceIndex);
@@ -31,6 +29,10 @@ public class SequenceHud : MonoBehaviour
 					GameObject.Find(imageName).GetComponent<Image>().sprite = Resources.Load<Sprite>(imagePath);
 				}
 			}
+			GameObject.Find("Ritual_" + PlayerIndex).SetActive(true);
+
+			string playerName = string.Format("Ritual_{0}/PlayerName", PlayerIndex);
+			GameObject.Find(playerName).GetComponent<Text>().text = "P" + PlayerIndex;
 		}
 		else
 		{
